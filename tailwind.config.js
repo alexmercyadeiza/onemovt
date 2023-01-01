40fe3fe9886fc61ad084759f248c74a2fe30149e
Adeiza <alexmercyadeiza@gmail.com>
2023-01-01 15:42:31 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        // sans: ['"Cabinet Grotesk"'],
+        sans: ["Figtree"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
